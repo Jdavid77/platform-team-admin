8a820bbe99e77e850ee7cbad1f2968981bb70457
Jdavid77 <johnynobrega17@gmail.com>
2026-07-19 17:17:45 +0100

    feat: add env reviewers logic

--- pulumi_repo_create.py
+++ pulumi_repo_create.py
@@ -1,6 +1,6 @@
 import yaml
 from pulumi import ResourceOptions, export
-from pulumi_github import Provider, Repository, Membership, BranchProtection, RepositoryEnvironment
+from pulumi_github import Provider, Repository, Membership, BranchProtection, RepositoryEnvironment, RepositoryEnvironmentReviewerArgs, get_user
 
 # def create_members(provider: Provider):
 #     with open("config/platform_team_values.yaml") as f:
@@ -47,10 +47,17 @@ def create_repos(provider: Provider):
         )
 
         for env_def in repo_def.get("environments", []):
+            reviewer_usernames = env_def.get("reviewers", [])
+            reviewers = []
+            if reviewer_usernames:
+                user_ids = [int(get_user(username=u).id) for u in reviewer_usernames]
+                reviewers = [RepositoryEnvironmentReviewerArgs(users=user_ids)]
+
             RepositoryEnvironment(
                 f"{repo_name}-{env_def['name']}-environment",
                 repository=repo_name,
                 environment=env_def["name"],
+                reviewers=reviewers,
                 opts=ResourceOptions(provider=provider, depends_on=[repository]),
             )
 
