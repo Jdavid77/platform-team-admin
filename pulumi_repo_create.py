b18b2128a638076b0193b139a5d7cc0a91fb7c7a
joao.nobrega <joao.nobrega@definedcrowd.com>
2026-07-19 15:48:53 +0100

    feat(github): add members logic ( commented )

--- pulumi_repo_create.py
+++ pulumi_repo_create.py
@@ -1,7 +1,25 @@
 import yaml
 from pulumi import ResourceOptions, export
-from pulumi_github import Provider, Repository
+from pulumi_github import Provider, Repository, Membership
 
+# def create_members(provider: Provider):
+#     with open("config/platform_team_values.yaml") as f:
+#         data = yaml.safe_load(f)
+
+#     for team_member in data.get("github_organization_members", []):
+#         name = team_member.get("name")
+#         username = team_member.get("github-username")
+#         role = team_member.get("github-role", "member")
+
+#     for member in data["github_organization_members"]:
+#         username = member["github-username"]
+#         role = member["github-role"]
+
+#         team_membership = Membership(
+#             f"github-membership-{username}",
+#             username=username,
+#             role=role,
+#         )
 
 def create_repos(provider: Provider):
     with open("config/platform_team_values.yaml") as f:
